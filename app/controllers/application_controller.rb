8fe3c28bbd47d7d52f94192ce52cf824ee678083
Kweeka <mtalbi29@yahoo.com>
2023-03-31 15:00:36 +0100

    Import secret key from .env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate
+    secret = ENV['JWT_SECRET_KEY']
     token = request.headers['Authorization']&.split&.last
     begin
-      payload = JWT.decode(token, 'my_secret_key', true, algorithm: 'HS256').first
+      payload = JWT.decode(token, secret, true, algorithm: 'HS256').first
       @current_user_id = payload['user_id']
     rescue JWT::DecodeError
       render json: { error: 'You are not allowed to access this api, please login' }, status: :unauthorized
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate
   def create
+    secret = ENV['JWT_SECRET_KEY']
     user = User.find_by(username: params[:username])
     if user
-      token = JWT.encode({ user_id: user.id }, 'my_secret_key', 'HS256')
+      token = JWT.encode({ user_id: user.id }, secret, 'HS256')
       render json: { token: }
     else
       render json: { error: 'Invalid username' }, status: :unauthorized
