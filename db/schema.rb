fdbc5f916e556007746252f1b08df597e864ae8a
Kweeka <mtalbi29@yahoo.com>
2023-03-31 19:11:32 +0100

    Add Car and Image models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_115251) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_174422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "color"
+    t.float "price"
+    t.date "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "car_id", null: false
+    t.index ["car_id"], name: "index_images_on_car_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_115251) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cars", "users"
+  add_foreign_key "images", "cars"
 end
