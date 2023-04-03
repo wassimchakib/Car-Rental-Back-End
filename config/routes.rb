bf2a5348ce456fbd13c04a84286ef5c8743ad314
Kweeka <mtalbi29@yahoo.com>
2023-04-03 11:24:29 +0100

    Update file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'reservations/create'
-  get 'reservations/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -11,11 +9,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :homes, only: [:index]
-      resources :reservations, only: [:create, :destroy]
+      resources :reservations, only: [:index, :create, :destroy]
     end
   end
 
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
-
 end
