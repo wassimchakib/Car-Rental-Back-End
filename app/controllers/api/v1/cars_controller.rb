e5510a9b1a458531236295fffa4a784d837ed51e
Profsain <profsaincrown@gmail.com>
2023-04-03 18:39:33 +0000

    add cars controller

---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::CarsController < ApplicationController
+  def index
+    @cars = Car.all
+    render json: @cars
+  end
+
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      render json: @car, status: :created
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    if @car.update(car_params)
+      render json: @car
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    head :no_content
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :description, :year, :color, :price)
+  end
+end
