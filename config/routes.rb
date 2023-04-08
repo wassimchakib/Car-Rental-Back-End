6805139022744ffb223de19dd17679085dd50edc
wassimchakib <wassimchakib@hotmail.com>
2023-04-08 23:34:33 +0000

    Change root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "api/v1/homes#index"
+  root to: redirect('/api-docs')
 
   namespace :api do
     namespace :v1 do
