58de05fec6385aca8e302442cc0e6878bc91b44a
Wassim <wassimchakib@hotmail.com>
2023-05-01 15:30:45 +0100

    Fix rake assets:precompile error

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '3.2.0'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4', '>= 7.0.4.3'
+gem 'sprockets-rails'
 
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module CarRentalBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.enabled = false
+
   end
 end
